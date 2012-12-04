1e283b390fdd74f39fc776eb624e8e6a097bd1d3
Mihai Maruseac <mihai@rosedu.org>
2012-12-04 20:07:35 +0200

    Refactor by treating zombies and plant alike.

--- pvz.hs
+++ pvz.hs
@@ -1,60 +1,33 @@
 import Test.QuickCheck
 
 {-
-Returns if a plant is alive, based on its life value.
+Returns if an entity is alive, based on its life value.
 -}
-isPlantAlive :: Int -> Bool
-isPlantAlive life = life > 0
+isEntityAlive :: Int -> Bool
+isEntityAlive life = life > 0
 
 {-
-Returns if a zombie is alive, based on its life value.
+Tests if an entity with zero life dies.
 -}
-isZombieAlive :: Int -> Bool
-isZombieAlive life = life > 0
+testEntityWithZeroLifeIsDead = not $ isEntityAlive 0
 
 {-
-Tests if a plant with zero life dies.
+Tests if an entity with positive life is alive.
 -}
-testPlantWithZeroLifeIsDead = not $ isPlantAlive 0
+testEntityWithLifeIsAlive life = life > 0 ==> isEntityAlive life
 
 {-
-Tests if a plant with positive life is alive.
+Tests if an entity with negative life is dead.
 -}
-testPlantWithLifeIsAlive life = life > 0 ==> isPlantAlive life
-
-{-
-Tests if a plant with negative life is dead.
--}
-testPlantWithNegativeLifeIsDead life = life < 0 ==> not $ isPlantAlive life
-
-{-
-Tests if a zombie with zero life dies.
--}
-testZombieWithZeroLifeIsDead = not $ isZombieAlive 0
-
-{-
-Tests if a zombie with positive life is alive.
--}
-testZombieWithLifeIsAlive life = life > 0 ==> isZombieAlive life
-
-{-
-Tests if a zombie with negative life is dead.
--}
-testZombieWithNegativeLifeIsDead life = life < 0 ==> not $ isZombieAlive life
+testEntityWithNegativeLifeIsDead life = life < 0 ==> not $ isEntityAlive life
 
 {-
 Test all properties in a single run.
 -}
 testAll = do
-  putStrLn "Testing testPlantWithZeroLifeIsDead"
-  quickCheck testPlantWithZeroLifeIsDead
-  putStrLn "Testing testPlantWithLifeIsAlive"
-  quickCheck testPlantWithLifeIsAlive
-  putStrLn "Testing testPlantWithNegativeLifeIsDead"
-  quickCheck testPlantWithNegativeLifeIsDead
-  putStrLn "Testing testZombieWithZeroLifeIsDead"
-  quickCheck testZombieWithZeroLifeIsDead
-  putStrLn "Testing testZombieWithLifeIsAlive"
-  quickCheck testZombieWithLifeIsAlive
-  putStrLn "Testing testZombieWithNegativeLifeIsDead"
-  quickCheck testZombieWithNegativeLifeIsDead
+  putStrLn "Testing testEntityWithZeroLifeIsDead"
+  quickCheck testEntityWithZeroLifeIsDead
+  putStrLn "Testing testEntityWithLifeIsAlive"
+  quickCheck testEntityWithLifeIsAlive
+  putStrLn "Testing testEntityWithNegativeLifeIsDead"
+  quickCheck testEntityWithNegativeLifeIsDead
