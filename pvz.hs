d43e96f2bf12369d8897bad6f19805815f749775
Mihai Maruseac <mihai@rosedu.org>
2012-12-04 19:41:08 +0200

    Add test for plant alive.

--- pvz.hs
+++ pvz.hs
@@ -7,3 +7,8 @@ testPlantWithZeroLifeIsDead = isPlantDead 0
   where
     isPlantDead :: Int -> Bool
     isPlantDead life = life == 0
+
+{-
+Tests if a plant with non-zero life is alive.
+-}
+testPlantWithLifeIsAlive = False
