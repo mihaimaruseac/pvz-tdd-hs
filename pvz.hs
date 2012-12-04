e6933daf12b376b67859ca3a4b0e72dd7c2d6c87
Mihai Maruseac <mihai@rosedu.org>
2012-12-04 20:41:51 +0200

    Extract functions for damages, position changes and position tests.

--- pvz.hs
+++ pvz.hs
@@ -12,6 +12,30 @@ isEntityAlive :: Life -> Bool
 isEntityAlive life = life > 0
 
 {-
+Modifies the position of a bullet of zombie.
+-}
+increasePosition :: Position -> Speed -> Position
+increasePosition position speed = position + speed
+
+{-
+Tests if a zombie is hit by a projectile.
+-}
+isZombieHit :: Position -> Position -> Bool
+isZombieHit pp zp = pp >= zp
+
+{-
+Returns if a zombie is near a plant.
+-}
+zombieNearPlant :: Position -> Bool
+zombieNearPlant zp = zp == 0
+
+{-
+Does a damage to one entity.
+-}
+damageEntity :: Life -> Damage -> Life
+damageEntity life damage = life - damage
+
+{-
 An entity with zero life dies.
 -}
 testEntityWithZeroLifeIsDead = not $ isEntityAlive 0
@@ -30,53 +54,31 @@ testEntityWithNegativeLifeIsDead life = life < 0 ==> not $ isEntityAlive life
 An entity should reduce the life of another entity.
 -}
 testEntityDamageEntity life damage = life - damage == damageEntity life damage
-  where
-    damageEntity :: Life -> Damage -> Life
-    damageEntity life damage = life - damage
 
 {-
 Second elapsed for entity: the position should increase by entity's speed.
 -}
 testSecondElapsed position speed = position + speed == increasePosition position speed
-  where
-    increasePosition :: Position -> Speed -> Position
-    increasePosition position speed = position + speed
 
 {-
 A projectile hits a zombie if its position is greater or equal than zombie's.
 -}
 testZombieIsShot pp zp = pp >= zp ==> isZombieHit pp zp
-  where
-    isZombieHit :: Position -> Position -> Bool
-    isZombieHit pp zp = pp >= zp
 
 {-
 When a zombie is hit, it's life is decreased.
 -}
 testZombieShot pp zp lifez dmgp = isZombieHit pp zp ==> lifez - dmgp == damageEntity lifez dmgp
-  where
-    damageEntity :: Life -> Damage -> Life
-    damageEntity life damage = life - damage
-    isZombieHit :: Position -> Position -> Bool
-    isZombieHit pp zp = pp >= zp
 
 {-
 A zombie can eat a plant if it is near it.
 -}
 testZombieCanEat zp = zp == 0 ==> zombieNearPlant zp
-  where
-    zombieNearPlant :: Position -> Bool
-    zombieNearPlant zp = zp == 0
 
 {-
 When a zombie is near the plant, he eats it.
 -}
 testZombieEatsPlant zp lifep dmgz = zombieNearPlant zp ==> lifep - dmgz == damageEntity lifep dmgz
-  where
-    damageEntity :: Life -> Damage -> Life
-    damageEntity life damage = life - damage
-    zombieNearPlant :: Position -> Bool
-    zombieNearPlant zp = zp == 0
 
 {-
 Test all properties in a single run.
