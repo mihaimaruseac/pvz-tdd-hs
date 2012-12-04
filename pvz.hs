4674f1d84f1424fcc0d8bd3824bccd2693781a11
Mihai Maruseac <mihai@rosedu.org>
2012-12-04 20:04:13 +0200

    Add tests and implementation for zombie life.

--- pvz.hs
+++ pvz.hs
@@ -7,6 +7,12 @@ isPlantAlive :: Int -> Bool
 isPlantAlive life = life > 0
 
 {-
+Returns if a zombie is alive, based on its life value.
+-}
+isZombieAlive :: Int -> Bool
+isZombieAlive life = life > 0
+
+{-
 Tests if a plant with zero life dies.
 -}
 testPlantWithZeroLifeIsDead = not $ isPlantAlive 0
@@ -22,6 +28,21 @@ Tests if a plant with negative life is dead.
 testPlantWithNegativeLifeIsDead life = life < 0 ==> not $ isPlantAlive life
 
 {-
+Tests if a zombie with zero life dies.
+-}
+testZombieWithZeroLifeIsDead = not $ isZombieAlive 0
+
+{-
+Tests if a zombie with positive life is alive.
+-}
+testZombieWithLifeIsAlive life = life > 0 ==> isZombieAlive life
+
+{-
+Tests if a zombie with negative life is dead.
+-}
+testZombieWithNegativeLifeIsDead life = life < 0 ==> not $ isZombieAlive life
+
+{-
 Test all properties in a single run.
 -}
 testAll = do
@@ -31,3 +52,9 @@ testAll = do
   quickCheck testPlantWithLifeIsAlive
   putStrLn "Testing testPlantWithNegativeLifeIsDead"
   quickCheck testPlantWithNegativeLifeIsDead
+  putStrLn "Testing testZombieWithZeroLifeIsDead"
+  quickCheck testZombieWithZeroLifeIsDead
+  putStrLn "Testing testZombieWithLifeIsAlive"
+  quickCheck testZombieWithLifeIsAlive
+  putStrLn "Testing testZombieWithNegativeLifeIsDead"
+  quickCheck testZombieWithNegativeLifeIsDead
